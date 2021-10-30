212a24185a812dd9cd99fa89470432a19d15ce8c
Michael Sutton <hicallmesutton@gmail.com>
2021-10-29 23:24:24 -0500

    made Login & SignUp be active forms.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,19 +1,23 @@
+import React, { useState } from 'react';
 
 function Login() {
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
 
   function handleOnSubmit(event) {
     event.preventDefault()
+    alert(`you have submited ${name}`)
   }
 
   return (
     <login>
       <h2>Login for Donator & Needy</h2>
-      <form name='chatBoxForm'>
+      <form name='chatBoxForm' onSubmit={event => handleOnSubmit(event)} >
         <label for='name'>Name:</label>
-        <input type='text' />
+        <input type='text' onChange={e => setName(e.target.value)} />
         <label for='password'>Password:</label>
-        <input type='text' />
-        <input type='submit' value='submit'/>
+        <input type='text' onChange={e => setPassword(e.target.value)} />
+        <input type='submit' value='submit' />
       </form>
     </login>
 
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,21 +1,36 @@
+// passdown props to adjust form for donator or needy
+import React, { useState } from 'react';
+
 function SignUp() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [about, setAbout] = useState();
+  const [password, setPassword] = useState();
+  const [passwordConfirm, setPasswordConfirm] = useState();
+
+  function handleOnSubmit(event) {
+    event.preventDefault()
+    alert(`${name} has been submitted`)
+  }
+
   return (
     <div id='content'>
       <h1>Sign Up Below</h1>
-      <form name='signUpForm'>
+      <form name='signUpForm' onSubmit={ e => handleOnSubmit(e)} >
         <label for='name'>Name:</label>
-        <input type='text' name='name' />
+        <input type='text' name='name' onChange={e => setName(e.target.value)} />
 
         <label for='email'>Email:</label>
-        <input type='text' name='email' />
+        <input type='text' name='email' onChange={e => setEmail(e.target.value)} />
 
         <label for='about'>About:</label>
-        <textarea name='about' />
+        <textarea name='about' onChange={e => setAbout(e.target.value)} />
 
         <label for='password'>Password:</label>
-        <input type='text' name='password' />
+        <input type='text' name='password' onChange={e => setPassword(e.target.value)} />
 
         <label for='passwor-confirm'>Password Confirmation</label>
+        <input type='text' name='password-confirm' onChange={e => setPasswordConfirm(e.target.value)} />
         <input type='submit' value='submit' />
       </form>
     </div>
