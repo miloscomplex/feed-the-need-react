8803f0e6c83ed0aa1217d80eff66f6a836222713
Michael Sutton <hicallmesutton@gmail.com>
2021-10-30 22:49:25 -0500

    CSS adjustments to landing page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -7,12 +7,16 @@ function Landing() {
       <h1>Welcome to Feed The Need</h1>
       <p class="emphasis">We, as a society, are more and more becoming cashless and dependent upon electronic transactions. A shortfall of this situation is that people in need are deprived; more frequently, people no longer have those couple of dollars or loose change in their purse or pocket to help them out.
       </p>
-      <subhead>Do you need help?</subhead>
-      <Link to='/sign-up'>Sign-up</Link>
-
-      <subhead>Do you want to help?</subhead>
-      <Link to='/sign-up'>Sign-up</Link>
-
+      <div class='cta-wrapper'>
+        <div class='cta'>
+          <subhead>Do you need help?</subhead>
+          <Link to='/sign-up'>Sign-up</Link>
+        </div>
+        <div class='cta'>
+          <subhead>Do you want to help?</subhead>
+          <Link to='/sign-up'>Sign-up</Link>
+        </div>
+      </div>
       <Login />
 
     </div>
