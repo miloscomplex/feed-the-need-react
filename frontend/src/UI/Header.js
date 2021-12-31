473ea505211a11e5a91fdf5da2a17d7311295453
Michael Sutton <hicallmesutton@gmail.com>
2021-12-31 00:28:21 -0600

    typographic adjustments

--- frontend/src/UI/Header.js
+++ frontend/src/UI/Header.js
@@ -5,7 +5,9 @@ function Header() {
   return (
     <header className='header'>
       <Nav />
-      <h1 class='header__landing-header'>Welcome to Feed The Need</h1>
+      <h1 class='header__landing-header'>
+        <div class='header__landing-header__eyebrow'>Welcome to</div> Feed The Need
+      </h1>
     </header>
   )
 }
