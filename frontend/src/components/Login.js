21884c9bbb216e1c9b9ff2f231169a3ce30a12ef
Michael Sutton <hicallmesutton@gmail.com>
2022-02-06 01:05:12 -0600

    working on login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,29 +1,61 @@
 import React, { useState } from 'react';
+import { API_ROOT, HEADERS } from '../constants'
 
 function Login() {
-  const [name, setName] = useState();
-  const [password, setPassword] = useState();
+
+  const [inputField, setInputField] = useState({
+    email: '',
+    password: ''
+  })
+
+  const inputsHandler = (e) => {
+    console.log(e.target)
+    setInputField( {[e.target.name]: e.target.value} )
+  }
 
   function handleOnSubmit(event) {
     event.preventDefault()
-    alert(`you have submited ${name}`)
+    fetch(`${API_ROOT}/users`, {
+      method: 'POST',
+      headers: HEADERS,
+      body: JSON.stringify(inputField)
+    })
+
+
+    setInputField({ email: '', password: '' })
+
   }
 
+
   return (
-    <login className='login'>
+    <div className='login'>
       <div className='login__wrapper'>
         <h2>Login for Donator & Needy</h2>
-        <form name='login__BoxForm' onSubmit={event => handleOnSubmit(event)} >
-          <label for='name'>Name:</label>
-          <input type='text' onChange={e => setName(e.target.value)} />
+        <form name='login__BoxForm' onSubmit={handleOnSubmit} >
+
+          <label for='email'>Email:</label>
+          <input type='text'
+            name='email'
+            onChange={inputsHandler}
+            value={inputField.email}
+          />
 
           <label for='password'>Password:</label>
-          <input type='text' onChange={e => setPassword(e.target.value)} />
+          <input type='text'
+            name='password'
+            onChange={inputsHandler}
+            value={inputField.password}
 
-          <input type='submit' value='submit' className='cta__btn' />
+          />
+
+          <input
+            type='submit'
+            value='submit'
+            className='cta__btn'
+          />
         </form>
       </div>
-    </login>
+    </div>
 
   )
 }
