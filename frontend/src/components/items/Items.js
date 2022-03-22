7aafbcd62cf8858a095c4b8402c629fb921df5b0
Michael Sutton <hicallmesutton@gmail.com>
2022-03-22 00:24:43 -0500

    added error handling to items fetch request

--- frontend/src/components/items/Items.js
+++ frontend/src/components/items/Items.js
@@ -2,6 +2,7 @@ import React, { Component, useState, useEffect } from 'react';
 import { connect } from 'react-redux'
 import { API_ROOT, HEADERS } from '../../constants';
 import { loadItems } from '../../redux/actions'
+import water from '../../images/water.jpg'
 
 function Items(props) {
 
@@ -16,15 +17,24 @@ function Items(props) {
             "Authorization": `Bearer ${token}`
           } 
         })
-          .then(resp => resp.json())
-          .then(data => 
-            setItemsList(data)
+          .then(resp => {
+            if (resp.status >= 400) {
+              throw new Error("Server responded with an error!")
+            }
+            return resp.json()
+            }
           )
+          .then(data => {
+            setItemsList(data)
+          }, 
+          err => {
+            console.log('an error has occured')
+          })
     },[])
 
     const itemArr = itemsList.map( item => 
       <li key={item.name} className='items__ul-li'>
-        <img className='items__ul__img' src='../../images/water.jpg' />
+        <img className='items__ul__img' src={water} />
         <h3>item: {item.name}</h3>
         <p>category: {item.category}</p>
         <a href='#' >click here to add</a>
@@ -33,7 +43,6 @@ function Items(props) {
 
     return (
         <div id='content' className='items space-above'>
-          <img className='items__ul__img' src='../../images/water.jpg' />
           <h1>All the items to choose from</h1>
             <p className='items__p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pellentesque ante ut elit semper lacinia. Duis mollis at nisl vel pretium. Ut vitae condimentum ipsum, a efficitur lorem. Quisque lectus diam, dictum at egestas at, tempor eu turpis. Cras congue risus sit amet pulvinar lacinia. Etiam eu tortor tellus. Vivamus eu dolor eget mi gravida sodales in ut leo. Nulla lobortis porttitor tempus.</p>
 
