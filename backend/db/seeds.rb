fe88d5de5421e0da5b150e6561ea4fc01e03d92f
Michael Sutton <hicallmesutton@gmail.com>
2022-04-09 21:53:19 -0500

    added seed data and rebuilt the db to reseed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,13 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Michael Dutton', password: '12345', email: 'mike@mike.com', user_type: 'needy', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim a ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum')
-User.create(name: 'James Dutton', password: '12345', email: 'james@james.com', user_type: 'needy', bio: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum')
+User.create(name: 'Michael Dutton', password: '12345', email: 'mike@mike.com', user_type: 'needy', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim a ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum', credits: '100')
+User.create(name: 'James Dutton', password: '12345', email: 'james@james.com', user_type: 'needy', bio: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum', credits: '100')
 User.create(name: 'Huey Gawker',  password: '12345', email: 'huey@huey.com', user_type: 'donator',  bio: 'Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum')
+User.create(name: 'Lisa Moore',  password: '12345', email: 'lisa@lisa.com', user_type: 'donator',  bio: 'Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum')
+
 
 Item.create(user_id: 2, name: 'coat', qty: 1, category: 'clothing')
 Item.create(user_id: 1, name: 'sleeping bag', qty: 1, category: 'essentials')
 Item.create(user_id: 1, name: 'shoes', qty: 2, category: 'clothing')
 Item.create(user_id: 2, name: 'gallon of water', qty: 4, category: 'clothing')
+Item.create(user_id: 3, name: 'pants', qty: 1, category: 'clothing')
+Item.create(user_id: 1, name: 'toothpaste and brush', qty: 1, category: 'essentials')
+Item.create(user_id: 1, name: 'shoes size 11', qty: 2, category: 'clothing')
+Item.create(user_id: 2, name: 'work pants', qty: 4, category: 'clothing')
+
 
 puts "database has been seeded successfully"
